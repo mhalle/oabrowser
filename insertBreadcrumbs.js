aa9e5c38389a705fcdbb40a55ec1eb5d5bbff2d3
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-02-29 22:02:01 -0500

    fix : typo
    Signed-off-by: Valentin Demeusy <valentin.demeusy@gmail.com>

--- insertBreadcrumbs.js
+++ insertBreadcrumbs.js
@@ -3,19 +3,19 @@ angular.module('atlasDemo').directive( 'insertBreadcrumbs', function () {
     return {
         restrict: 'A',
         scope: {},
-        templateUrl : "templates/breadcrumbs.html"
+        templateUrl : "templates/breadcrumbs.html",
         controller: function ( $scope, $element, $rootScope ) {
-        $scope = {
-        data : {
-        breadcrumbs : []
-    }
-};
-                                      $rootScope.$on('insertBreadcrumbs', function (event, breadcrumbs) {
-    $scope.data = {
-        breadcrumbs : breadcrumbs
+            $scope = {
+                data : {
+                    breadcrumbs : []
+                }
+            };
+            $rootScope.$on('insertBreadcrumbs', function (event, breadcrumbs) {
+                $scope.data = {
+                    breadcrumbs : breadcrumbs
+                };
+                $scope.$apply();
+            });
+        }
     };
-    $scope.$apply();
-});
-}
-};
 });
