774119167dc615da409d31b859ec86676f20a271
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-03-18 22:10:55 -0400

    fix : first try at fixing slices orientation on the left panel

--- ng-directives/insertSliceDirective.js
+++ ng-directives/insertSliceDirective.js
@@ -5,12 +5,13 @@ angular.module('atlasDemo').directive( 'insertSlice', function () {
         scope: { sliceId : '=sliceid' },
         controller: function ( $scope, $element, mainApp ) {
 
+            $scope.sliceId = $element.attr('sliceid');
 
             mainApp.on('insertSlice', function (data) {
                 //we are expecting data to hold two properties :
                 //      - sliceId which identify in which directive you want to insert the slice
                 //      - slice the actual slice object
-                if ($scope.sliceId === data.sliceId || $element.attr('sliceid')=== data.sliceId) {
+                if ($scope.sliceId === data.sliceId) {
                     $scope.slice = data.slice;
                     var canvas = document.createElement('canvas');
                     $element.append(canvas);
@@ -37,12 +38,31 @@ angular.module('atlasDemo').directive( 'insertSlice', function () {
 
                     var zoom = Math.min(canvas.width/image.width, canvas.height/image.height);
 
+                    ctx.save();
+                    ctx.translate(canvas.width/2 canvas.height/2);
+                    if ($scope.sliceId === 'axial') {
+
+                        ctx.scale(-1,1);
+
+                    } else if ($scope.sliceId === 'coronal') {
+
+                        ctx.scale(-1,-1);
+
+                    } else if ($scope.sliceId === 'sagittal') {
+
+                        ctx.rotate(Math.PI/2)
+                        ctx.scale(-1,1);
+
+                    }
+
                     ctx.drawImage(image,
-                                  canvas.width/2-zoom*image.width/2,
-                                  canvas.height/2-zoom*image.height/2,
+                                  -zoom*image.width/2,
+                                  -zoom*image.height/2,
                                   zoom*image.width,
                                   zoom*image.height
                                  );
+
+                    ctx.restore();
 
 
                 }
