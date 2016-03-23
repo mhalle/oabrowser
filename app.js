972a801a8bf5d5c8648a1755db3c5201c558a77e
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-03-22 20:49:53 -0400

    add : slicer like background

--- app.js
+++ app.js
@@ -174,7 +174,8 @@ angular.module('atlasDemo').run(["mainApp", "objectSelector", function (mainApp,
         camera.aspect = container.clientWidth / container.clientHeight;
         camera.updateProjectionMatrix();
 
-        renderer = new THREE.WebGLRenderer( { antialias: false } );
+        renderer = new THREE.WebGLRenderer( { antialias: false, alpha : true } );
+        renderer.setClearColor( 0x000000, 0 );
         renderer.setPixelRatio( window.devicePixelRatio );
         renderer.setSize( container.clientWidth, container.clientHeight );
 
