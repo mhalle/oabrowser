890e415476f098271a0dd5b0fcc9ebab0876fd3f
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-04-09 05:21:53 -0400

    fix : issues with popover

--- angularInit.js
+++ angularInit.js
@@ -1,2 +1,2 @@
-angular.module('atlasDemo', [ 'ngSanitize', 'adaptv.adaptStrap', 'ui.bootstrap', 'ui.layout' ]);
+angular.module('atlasDemo', [ 'ngSanitize', 'adaptv.adaptStrap', 'ui.bootstrap', 'ui.layout', 'rzModule' ]);
 
