49acb8c034765906651d7b35eaf7803761447086
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-05-31 15:41:11 -0400

    fix : use browserified version of service worker to support firefox

--- sw.js
+++ sw.js
@@ -1,160 +1,161 @@
-self.CACHE_NAME =  'atlas-viewer';
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+    // Set the CACHE_NAME first
+    self.CACHE_NAME = 'atlas-viewer';
 
-console.log('Service started');
-self.addEventListener('install', function(event) {
-    //self.skipWaiting();
-    console.log('Installed', event);
-});
+    // and require the sw-cache-all
+    require('../../index.js');
 
-// caches polyfill because it is not added to native yet!
-if (!Cache.prototype.add) {
-    Cache.prototype.add = function add(request) {
-        return this.addAll([request]);
-    };
-}
+},{"../../index.js":2}],2:[function(require,module,exports){
+    // caches polyfill because it is not added to native yet!
+    var caches = require('./lib/serviceworker-caches');
 
-if (!Cache.prototype.addAll) {
-    Cache.prototype.addAll = function addAll(requests) {
-        var cache = this;
+    if(typeof self.CACHE_NAME !== 'string') {
+        throw new Error('Cache Name cannot be empty');
+    }
 
-        // Since DOMExceptions are not constructable:
-        function NetworkError(message) {
-            this.name = 'NetworkError';
-            this.code = 19;
-            this.message = message;
-        }
-        NetworkError.prototype = Object.create(Error.prototype);
+    self.addEventListener('fetch', function(event) {
 
-        return Promise.resolve().then(function() {
-            if (arguments.length < 1) {
-                throw new TypeError();
-            }
+        // Clone the request for fetch and cache
+        // A request is a stream and can be consumed only once.
+        var fetchRequest = event.request.clone(),
+            cacheRequest = event.request.clone();
 
-            // Simulate sequence<(Request or USVString)> binding:
-            var sequence = [];
+        // Respond with content from fetch or cache
+        event.respondWith(
 
-            requests = requests.map(function(request) {
-                if (request instanceof Request) {
-                    return request;
-                }
-                else {
-                    return String(request); // may throw TypeError
-                }
-            });
+            // Try fetch
+            fetch(fetchRequest)
 
-            return Promise.all(
-                requests.map(function(request) {
-                    if (typeof request === 'string') {
-                        request = new Request(request);
-                    }
+            // when fetch is successful, we update the cache
+            .then(function(response) {
 
-                    var scheme = new URL(request.url).protocol;
+                // A response is a stream and can be consumed only once.
+                // Because we want the browser to consume the response,
+                // as well as cache to consume the response, we need to
+                // clone it so we have 2 streams
+                var responseToCache = response.clone();
 
-                    if (scheme !== 'http:' && scheme !== 'https:') {
-                        throw new NetworkError("Invalid scheme");
-                    }
+                // and update the cache
+                caches
+                    .open(self.CACHE_NAME)
+                    .then(function(cache) {
 
-                    return fetch(request.clone());
-                })
-            );
-        }).then(function(responses) {
-            // TODO: check that requests don't overwrite one another
-            // (don't think this is possible to polyfill due to opaque responses)
-            return Promise.all(
-                responses.map(function(response, i) {
-                    return cache.put(requests[i], response);
-                })
-            );
-        }).then(function() {
-            return undefined;
-        });
-    };
-}
+                    // Clone the request again to use it
+                    // as the key for our cache
+                    var cacheSaveRequest = event.request.clone();
+                    cache.put(cacheSaveRequest, responseToCache);
 
-if (!CacheStorage.prototype.match) {
-    // This is probably vulnerable to race conditions (removing caches etc)
-    CacheStorage.prototype.match = function match(request, opts) {
-        var caches = this;
-
-        return this.keys().then(function(cacheNames) {
-            var match;
-
-            return cacheNames.reduce(function(chain, cacheName) {
-                return chain.then(function() {
-                    return match || caches.open(cacheName).then(function(cache) {
-                        return cache.match(request, opts);
-                    }).then(function(response) {
-                        match = response;
-                        return match;
-                    });
                 });
-            }, Promise.resolve());
-        });
-    };
-}
 
+                // Return the response stream to be consumed by browser
+                return response;
 
-var caches = self.caches;
+            })
 
-if(typeof self.CACHE_NAME !== 'string') {
-    throw new Error('Cache Name cannot be empty');
-}
+            // when fetch times out or fails
+            .catch(function(err) {
 
-self.addEventListener('fetch', function(event) {
-    // Clone the request for fetch and cache
-    // A request is a stream and can be consumed only once.
-    var fetchRequest = event.request.clone(),
-        cacheRequest = event.request.clone();
+                // Return the promise which
+                // resolves on a match in cache for the current request
+                // ot rejects if no matches are found
+                return caches.match(cacheRequest);
 
-    // Respond with content from fetch or cache
-    event.respondWith(
+            })
+        );
+    });
 
-        // Try fetch
-        fetch(fetchRequest)
+    // Now we need to clean up resources in the previous versions
+    // of Service Worker scripts
+    self.addEventListener('activate', function(event) {
 
-        // when fetch is successful, we update the cache
-        .then(function(response) {
+        // Destroy the cache
+        event.waitUntil(caches.delete(self.CACHE_NAME));
 
-            // A response is a stream and can be consumed only once.
-            // Because we want the browser to consume the response,
-            // as well as cache to consume the response, we need to
-            // clone it so we have 2 streams
-            var responseToCache = response.clone();
+    });
+},{"./lib/serviceworker-caches":3}],3:[function(require,module,exports){
+    if (!Cache.prototype.add) {
+        Cache.prototype.add = function add(request) {
+            return this.addAll([request]);
+        };
+    }
 
-            // and update the cache
-            caches
-                .open(self.CACHE_NAME)
-                .then(function(cache) {
+    if (!Cache.prototype.addAll) {
+        Cache.prototype.addAll = function addAll(requests) {
+            var cache = this;
 
-                // Clone the request again to use it
-                // as the key for our cache
-                var cacheSaveRequest = event.request.clone();
-                cache.put(cacheSaveRequest, responseToCache);
+            // Since DOMExceptions are not constructable:
+            function NetworkError(message) {
+                this.name = 'NetworkError';
+                this.code = 19;
+                this.message = message;
+            }
+            NetworkError.prototype = Object.create(Error.prototype);
 
+            return Promise.resolve().then(function() {
+                if (arguments.length < 1) throw new TypeError();
+
+                // Simulate sequence<(Request or USVString)> binding:
+                var sequence = [];
+
+                requests = requests.map(function(request) {
+                    if (request instanceof Request) {
+                        return request;
+                    }
+                    else {
+                        return String(request); // may throw TypeError
+                    }
+                });
+
+                return Promise.all(
+                    requests.map(function(request) {
+                        if (typeof request === 'string') {
+                            request = new Request(request);
+                        }
+
+                        var scheme = new URL(request.url).protocol;
+
+                        if (scheme !== 'http:' && scheme !== 'https:') {
+                            throw new NetworkError("Invalid scheme");
+                        }
+
+                        return fetch(request.clone());
+                    })
+                );
+            }).then(function(responses) {
+                // TODO: check that requests don't overwrite one another
+                // (don't think this is possible to polyfill due to opaque responses)
+                return Promise.all(
+                    responses.map(function(response, i) {
+                        return cache.put(requests[i], response);
+                    })
+                );
+            }).then(function() {
+                return undefined;
             });
+        };
+    }
 
-            // Return the response stream to be consumed by browser
-            return response;
+    if (!CacheStorage.prototype.match) {
+        // This is probably vulnerable to race conditions (removing caches etc)
+        CacheStorage.prototype.match = function match(request, opts) {
+            var caches = this;
 
-        })
+            return this.keys().then(function(cacheNames) {
+                var match;
 
-        // when fetch times out or fails
-        .catch(function(err) {
+                return cacheNames.reduce(function(chain, cacheName) {
+                    return chain.then(function() {
+                        return match || caches.open(cacheName).then(function(cache) {
+                            return cache.match(request, opts);
+                        }).then(function(response) {
+                            match = response;
+                            return match;
+                        });
+                    });
+                }, Promise.resolve());
+            });
+        };
+    }
 
-            // Return the promise which
-            // resolves on a match in cache for the current request
-            // ot rejects if no matches are found
-            return caches.match(cacheRequest);
-
-        })
-    );
-});
-
-// Now we need to clean up resources in the previous versions
-// of Service Worker scripts
-self.addEventListener('activate', function(event) {
-
-    // Destroy the cache
-    event.waitUntil(caches.delete(self.CACHE_NAME));
-
-});
+    module.exports = self.caches;
+},{}]},{},[1]);
