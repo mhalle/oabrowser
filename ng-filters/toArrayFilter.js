d552a132a32d560972752c7dd5f335bf53e67605
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-06-01 19:58:55 -0400

    add : toArray filter to fix order by

---
+++ ng-filters/toArrayFilter.js
@@ -0,0 +1,13 @@
+angular.module('atlasDemo').filter('toArray', function () {
+    'use strict';
+
+    return function (obj) {
+        if (!(obj instanceof Object)) {
+            return obj;
+        }
+
+        return Object.keys(obj).map(function (key) {
+            return Object.defineProperty(obj[key], '$key', {__proto__: null, value: key});
+        });
+    };
+});
