cd9d4b7935599ab2342780cfce364166be5aa039
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-06-03 19:42:02 -0400

    enh : doc toArray filter

--- ng-filters/toArrayFilter.js
+++ ng-filters/toArrayFilter.js
@@ -1,3 +1,4 @@
+//this filter converts a dictionnary of object to an array of object but keep the key as the $key attribute of each object
 angular.module('atlasDemo').filter('toArray', function () {
     'use strict';
 
@@ -7,6 +8,7 @@ angular.module('atlasDemo').filter('toArray', function () {
         }
 
         return Object.keys(obj).map(function (key) {
+            //using define property allows us to create a constant property.
             return Object.defineProperty(obj[key], '$key', {__proto__: null, value: key});
         });
     };
