f8ba7b3cd06df8865429f303c4a50548fc032f3b
Michael Halle <m -@- halle.us>
2016-10-11 23:43:46 -0400

    STYLE: extraneous blank lines

--- ng-providers/objectSelectorProvider.js
+++ ng-providers/objectSelectorProvider.js
@@ -7,10 +7,8 @@ angular.module('atlasDemo').provider('objectSelector', ['mainAppProvider', funct
         singleton = {};
 
     function _select (object) {
-
         var mesh = object.mesh;
         if (object['@type'] === 'Structure') {
-
             if (!mesh.originalColor) {
                 mesh.originalColor = mesh.material.color.getHex();
             }
@@ -18,84 +16,58 @@ angular.module('atlasDemo').provider('objectSelector', ['mainAppProvider', funct
 
         }
         else if (object['@type'] === 'Group') {
-
             for (var i = 0; i < object.member.length; i++) {
-
                 removeFromSelection(object.member[i]);
                 _select(object.member[i]);
-
             }
-
         }
         else {
-
             throw 'The given object is not selectable';
-
         }
-
         object.selected = true;
     }
-
     function _unselect (object) {
-
         if (object['@type'] === 'Structure') {
-
             object.mesh.material.color.setHex(object.mesh.originalColor);
-
         }
         else if (object['@type'] === 'Group') {
-
             for (var i = 0; i < object.member.length; i++) {
-
                 _unselect(object.member[i]);
-
             }
-
         }
-
         object.selected = false;
     }
 
     function select (object) {
-
         clearSelection();
         _select(object);
         selectedObjects = [object];
         mainApp.emit('objectSelected', object);
-
     }
 
     function clearSelection () {
-
         for (var i = 0; i < selectedObjects.length; i++) {
             _unselect(selectedObjects[i]);
         }
         selectedObjects = [];
         mainApp.emit('selectionCleared');
-
     }
 
     function addToSelection (object) {
-
         if (!object.selected) {
             _select(object);
             selectedObjects.push(object);
             mainApp.emit('objectAddedToSelection', selectedObjects);
         }
-
     }
 
     function removeFromSelection (object) {
-
         if (object.selected) {
-
             var index = selectedObjects.indexOf(object);
             if (index > -1) {
-
                 selectedObjects.splice(index,1);
                 _unselect(object);
                 mainApp.emit('objectRemovedFromSelection', selectedObjects);
-
             }
         }
     }
@@ -104,7 +76,6 @@ angular.module('atlasDemo').provider('objectSelector', ['mainAppProvider', funct
     singleton.clearSelection = clearSelection;
     singleton.addToSelection = addToSelection;
     singleton.removeFromSelection = removeFromSelection;
-
 
     Object.defineProperty(singleton, 'selectedObjects', {
         get :  function () {
@@ -119,7 +90,6 @@ angular.module('atlasDemo').provider('objectSelector', ['mainAppProvider', funct
                 for (var i = 0; i < value.length; i++) {
                     addToSelection(value[i]);
                 }
-
             }
             else {
                 throw 'Type error : please check the type of the object you want to select';
