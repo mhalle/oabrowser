e6776ceffe55c0927180aad945044af99dfada16
Michael Halle <m -@- halle.us>
2016-10-22 01:23:06 -0400

    BUG revamp screenshots and thumbnail code

--- src/ng-providers/screenshotSceneProvider.js
+++ src/ng-providers/screenshotSceneProvider.js
@@ -1,5 +1,6 @@
 
 const angular = require('angular');
+const FileSaver = require('file-saver');
 
 angular.module('atlasDemo').provider('screenshotScene', ['mainAppProvider', function (mainAppProvider) {
 
@@ -28,64 +29,37 @@ angular.module('atlasDemo').provider('screenshotScene', ['mainAppProvider', func
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
-    function resizeBase64Img(base64, iwidth, iheight, width, height) {
+    function saveScreenshot() {
+        var sourceCanvas = document.querySelector('#rendererFrame canvas');
+        var destCanvas = resizeCanvasWithBackground (sourceCanvas);
+        exportable.base64 = destCanvas.toDataURL();
+        setTimeout(function() {
+            firebaseView.commit('screenshot');
+            mainApp.emit('screenshotScene.commited');
+        }, 0);
+    }
+
+    function resizeCanvasWithBackground (sourceCanvas, size) {
+        var size = size || 128;
+        var width = size;
+        var height = size;
         var canvas = document.createElement("canvas");
         canvas.width = width;
         canvas.height = height;
         var context = canvas.getContext("2d");
         drawLinearGradient(context, canvas);
-        var deferred = $.Deferred();
-        var ratio = Math.min(iwidth/width, iheight/height);
-        $("<img/>").attr("src",  base64).load(function() {
-            context.drawImage(this, iwidth/2-ratio*width/2, iheight/2-ratio*height/2, ratio*width, ratio*height, 0,0,width, height);
-            deferred.resolve(canvas.toDataURL());
-        });
-        return deferred.promise();
+        var ratio = Math.min(sourceCanvas.width/width, sourceCanvas.height/height);
+        context.drawImage(sourceCanvas, sourceCanvas.width/2-ratio*width/2, 
+                sourceCanvas.height/2-ratio*height/2, 
+                ratio*width, ratio*height, 0, 0, width, height);
+        return canvas;
     }
-
-    function commitScreenshot (screenshot) {
-        exportable.base64 = screenshot;
-        firebaseView.commit('screenshot');
-        mainApp.emit('screenshotScene.commited');
-    }
-
-    function saveScreenshot (size) {
-        size = size || 128;
-        var canvas = document.querySelector('#rendererFrame canvas');
-        resizeBase64Img(canvas.toDataURL(), canvas.width, canvas.height, size, size).then(commitScreenshot);
-    }
-
-    function getBase64WithBackground (originalCanvas) {
-        var canvas = document.createElement("canvas");
-        canvas.width = originalCanvas.width;
-        canvas.height = originalCanvas.height;
-        var context = canvas.getContext("2d");
-        drawLinearGradient(context, canvas);
-        var deferred = $.Deferred();
-        $("<img/>").attr("src",  originalCanvas.toDataURL()).load(function() {
-            context.drawImage(this, 0,0);
-            deferred.resolve(canvas.toDataURL());
-        });
-        return deferred.promise();
-    }
-
-    function triggerDownload (screenshot) {
-        var link = document.createElement('a');
-        link.href = screenshot;
-        link.download = 'screenshot.png';
-        document.body.appendChild(link);
-        link.click();
-
-        //garbage collection in the DOM
-        setTimeout(function () {
-            document.body.removeChild(link);
-        },10);
-    }
-
 
     function downloadScreenshot () {
         var canvas = document.querySelector('#rendererFrame canvas');
-        getBase64WithBackground(canvas).then(triggerDownload);
+        canvas.toBlob(function(blob) {
+            FileSaver.saveAs(blob, 'atlas-screenshot.png');
+        });
     }
 
     singleton.setFirebaseView = setFirebaseView;
